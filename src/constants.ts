271210cc6391e7a333671b690684164c5112c249
Mustafa Ucuncu <mustafaucuncu@onet.pl>
2020-03-10 18:37:59 +0100

    Add production urls

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
 // Production urls to be confirmed
-export const SERVICE = 'https://wyszukiwarkaregontest.stat.gov.pl/wsBIR/UslugaBIRzewnPubl.svc';
+export const SERVICE = 'https://wyszukiwarkaregon.stat.gov.pl/wsBIR/UslugaBIRzewnPubl.svc';
 export const SERVICE_TEST = 'https://wyszukiwarkaregontest.stat.gov.pl/wsBIR/UslugaBIRzewnPubl.svc';
 
-export const WSDL = 'https://wyszukiwarkaregontest.stat.gov.pl/wsBIR/wsdl/UslugaBIRzewnPubl-ver11-test.wsdl';
+export const WSDL = 'https://wyszukiwarkaregon.stat.gov.pl/wsBIR/wsdl/UslugaBIRzewnPubl-ver11-prod.wsdl';
 export const WSDL_TEST = 'https://wyszukiwarkaregontest.stat.gov.pl/wsBIR/wsdl/UslugaBIRzewnPubl-ver11-test.wsdl';
 
 export const ACTION = 'http://CIS/BIR/PUBL/2014/07/IUslugaBIRzewnPubl/';
