ad541d1cbe781de8af8e9851f6056ade3abf357f
Mustafa Ucuncu <mustafaucuncu@onet.pl>
2020-03-17 08:41:53 +0100

    Compile

--- dist/constants.d.ts
+++ dist/constants.d.ts
@@ -1,6 +1,6 @@
-export declare const SERVICE = "https://wyszukiwarkaregontest.stat.gov.pl/wsBIR/UslugaBIRzewnPubl.svc";
+export declare const SERVICE = "https://wyszukiwarkaregon.stat.gov.pl/wsBIR/UslugaBIRzewnPubl.svc";
 export declare const SERVICE_TEST = "https://wyszukiwarkaregontest.stat.gov.pl/wsBIR/UslugaBIRzewnPubl.svc";
-export declare const WSDL = "https://wyszukiwarkaregontest.stat.gov.pl/wsBIR/wsdl/UslugaBIRzewnPubl-ver11-test.wsdl";
+export declare const WSDL = "https://wyszukiwarkaregon.stat.gov.pl/wsBIR/wsdl/UslugaBIRzewnPubl-ver11-prod.wsdl";
 export declare const WSDL_TEST = "https://wyszukiwarkaregontest.stat.gov.pl/wsBIR/wsdl/UslugaBIRzewnPubl-ver11-test.wsdl";
 export declare const ACTION = "http://CIS/BIR/PUBL/2014/07/IUslugaBIRzewnPubl/";
 export declare const ACTION_ZALOGUJ = "Zaloguj";
--- dist/constants.js
+++ dist/constants.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 // Production urls to be confirmed
-exports.SERVICE = 'https://wyszukiwarkaregontest.stat.gov.pl/wsBIR/UslugaBIRzewnPubl.svc';
+exports.SERVICE = 'https://wyszukiwarkaregon.stat.gov.pl/wsBIR/UslugaBIRzewnPubl.svc';
 exports.SERVICE_TEST = 'https://wyszukiwarkaregontest.stat.gov.pl/wsBIR/UslugaBIRzewnPubl.svc';
-exports.WSDL = 'https://wyszukiwarkaregontest.stat.gov.pl/wsBIR/wsdl/UslugaBIRzewnPubl-ver11-test.wsdl';
+exports.WSDL = 'https://wyszukiwarkaregon.stat.gov.pl/wsBIR/wsdl/UslugaBIRzewnPubl-ver11-prod.wsdl';
 exports.WSDL_TEST = 'https://wyszukiwarkaregontest.stat.gov.pl/wsBIR/wsdl/UslugaBIRzewnPubl-ver11-test.wsdl';
 exports.ACTION = 'http://CIS/BIR/PUBL/2014/07/IUslugaBIRzewnPubl/';
 exports.ACTION_ZALOGUJ = 'Zaloguj';
